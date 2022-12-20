de4db2c1a0ee85dbe2d6832d3a642219703d75d3
slava <slavasavko23@gmail.com>
2022-12-20 12:16:48 +0300

    seminar

---
+++ sem6/num41.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел");
+int numberM = int.Parse(Console.ReadLine()!);
+
+int[] firstArray = CollectNumbers(numberM);
+
+PrintArray(firstArray);
+
+Console.WriteLine(CountPositive(firstArray));
+
+int[] CollectNumbers(int size)
+{
+    int[] array = new int[size];
+    Console.WriteLine($"Начинайте ввод {size} чисел(а)");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountPositive(int[] array)
+{
+    int countPos = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            countPos++;
+        }
+    }
+    return countPos;
+}
---
+++ sem6/num43.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double xFromFunction = FindX(b1, k1, b2, k2);
+double yFromFunction = FindY(xFromFunction, k1, b1);
+
+Console.WriteLine($"Координаты точки пересечения двух прямых: ({xFromFunction};{yFromFunction})");
+
+double FindX(double b1f, double k1f, double b2f, double k2f)
+{
+    double calculatedX = Math.Round((b2f - b1f) / (k1f - k2f), 1);
+    return calculatedX;
+}
+
+double FindY(double calculatedX, double k1f, double b1f)
+{
+    double calculatedY = Math.Round(calculatedX * k1f + b1f, 1);
+    return calculatedY;
+}
