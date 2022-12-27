87e9ef295220c1b0f50d06bab258a548f91f3b8a
slava <slavasavko23@gmail.com>
2022-12-27 21:07:54 +0300

    seminar

---
+++ sem9/num66.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите число M:");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Summa(m, n));
+
+int Summa(int m, int n)
+{
+    if (m == n) return n;
+    else return m + Summa(m + 1, n);
+}
---
+++ sem9/num68.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Ackermann(m, n));
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
